6c5c0d86eace290c1db0a19823d923138dadd81c
Seu Nome <seu@email.com>
2025-05-21 10:40:35 -0300

    Adiciona endpoint teste na API e integração com react

---
+++ backend/Performetric.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Performetric.API.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Api ta ok" });
+        }
+    }
+}
