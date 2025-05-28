64caa49aef6f348927b37020081bb0f2d970d66d
Paulo Sergio <contato.paulofws@gmail.com>
2025-05-28 17:59:18 -0300

    feat(login): add prototype of header-login component

---
+++ frontend/frontend_viteversion/src/components/login/header-login.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import LogoLight from "../../assets/images/logo/logo-light.png";
+import LogoDark from "../../assets/images/logo/logo-dark.png";
+import { Moon, Sun } from "lucide-react";
+
+type Theme = "light" | "dark";
+
+export function HeaderLogin() {
+  const [theme, setTheme] = useState<Theme>("light");
+  const currentTheme = (prev: Theme) => (prev === "dark" ? "light" : "dark");
+
+  const toggleTheme = () => {
+    setTheme(currentTheme);
+    document.documentElement.classList.toggle("dark");
+  };
+
+  return (
+    <header
+      className="flex items-center justify-between max-w-screen-xl mx-auto py-4 px-2 sm:py-6 sm:px-6 lg:px-8"
+      aria-label="Header for login page"
+      role="banner"
+    >
+      <img
+        src={theme === "dark" ? LogoDark : LogoLight}
+        alt="performetric logo"
+        className="w-40"
+      />
+      <button
+        onClick={toggleTheme}
+        className="p-2 rounded-full bg-slate-700 hover:bg-slate-600 transition-colors"
+        aria-label="Toggle theme"
+        role="switch"
+      >
+        {theme === "dark" ? (
+          <Sun className="size-6 text-yellow-400" />
+        ) : (
+          <Moon className="size-6 text-slate-50" />
+        )}
+      </button>
+    </header>
+  );
+}
