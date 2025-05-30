118917be474582f668bf244de2d6ef8d691b04df
Seu Nome <seu@email.com>
2025-05-29 21:33:03 -0300

    BACK-END LOGIN API CONNECTED WITH FRONT-END + DATABASE

--- backend/Performetric.API/obj/Debug/net9.0/Performetric.API.AssemblyInfo.cs
+++ backend/Performetric.API/obj/Debug/net9.0/Performetric.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Performetric.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ca45ea787b97bd35b1e74bab2e29f3ca51a488c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aafe9fcfbfbcc38ede60c585eb0139cc3c09d6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Performetric.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Performetric.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
