a920c56ce00717d9dbd875838f97638f788812bf
Paulo Sergio <contato.paulofws@gmail.com>
2025-06-02 16:22:18 -0300

    fix(layout): fix height

--- frontend/frontend_viteversion/src/components/Layout.tsx
+++ frontend/frontend_viteversion/src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "./sidebar";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen bg-slate-100 dark:bg-slate-900 transition-colors duration-300">
+    <div className="flex min-h-screen bg-slate-100 dark:bg-slate-900 transition-colors duration-300">
       <Sidebar />
       {children}
     </div>
