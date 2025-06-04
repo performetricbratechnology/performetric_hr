f6e8fa13e859ed9606a5ecd6f422145178b05f3a
Paulo Sergio <contato.paulofws@gmail.com>
2025-06-04 15:23:18 -0300

    fix(layout): add overflow-hidden in main

--- frontend/frontend_viteversion/src/components/Layout.tsx
+++ frontend/frontend_viteversion/src/components/Layout.tsx
@@ -5,7 +5,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-slate-100 dark:bg-slate-900 transition-colors duration-300">
       <Sidebar />
-      <main className="flex-1 ml-14 sm:ml-18">{children}</main>
+      <main className="flex-1 ml-14 sm:ml-18 overflow-hidden">{children}</main>
     </div>
   );
 }
