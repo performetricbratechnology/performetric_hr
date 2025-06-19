18d94cc9fe64a944cf7b4285f93fbb1085b89305
Paulo Sergio <contato.paulofws@gmail.com>
2025-06-19 19:11:50 -0300

    refactor: using ui components

--- frontend/frontend_viteversion/src/screens/development-plan.tsx
+++ frontend/frontend_viteversion/src/screens/development-plan.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Tab, Tabs } from "../components/ui/tabs";
+import { Tab, Tabs, Description, Title } from "@/components/ui";
 import type { DevelopmentPlanType } from "../types/tabs";
 import { NewGoal } from "../components/development-plan/new-goal";
 import { Suggestions } from "../components/development-plan/suggestions";
@@ -11,18 +11,10 @@ export default function DevelopmentPlanPage() {
   return (
     <div className="container mx-auto pt-8 px-2 sm:px-4 md:px-6">
       <div className="mb-8">
-        <h1
-          className="text-3xl font-bold text-slate-900 dark:text-slate-100 mb-2"
-          aria-label="Plano de Desenvolvimento Individual"
-        >
-          Plano de Desenvolvimento Individual
-        </h1>
-        <p
-          className="text-slate-600 dark:text-slate-400"
-          aria-label="Gerencie suas metas e acompanhe seu desenvolvimento profissional"
-        >
+        <Title>Plano de Desenvolvimento Individual</Title>
+        <Description aria="Gerencie suas metas e acompanhe seu desenvolvimento profissional">
           Gerencie suas metas e acompanhe seu desenvolvimento profissional
-        </p>
+        </Description>
       </div>
       <Tabs>
         <Tab
